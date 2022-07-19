bc0a28d0844706048dcb24fa34799bbb2c1d85ef
crazyUtkarsh <95992213+crazyUtkarsh@users.noreply.github.com>
2022-07-19 17:18:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.round(Math.random()* 5);
 var t = Math.ceil(Math.random()* 6);
-document.querySelector(".img1").setAttribute("src","images/dice"+t+".png");
+document.querySelector(".img1").setAttribute("src","dice"+t+".png");
 var q = Math.ceil(Math.random()* 6);
-document.querySelector(".img2").setAttribute("src","images/dice"+q+".png");
+document.querySelector(".img2").setAttribute("src","dice"+q+".png");
 if (t===q){
   document.querySelector("h1").textContent = "Draw!";
 }
